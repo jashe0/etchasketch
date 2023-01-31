d9bd812dba6eb037d31e56c42fbb66d14542178c
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-01-31 18:55:26 +0000

    add reset function to clear board

--- script.js
+++ script.js
@@ -49,3 +49,8 @@ function colorDiv(){
 function setColor(colorChoice){
     color = colorChoice ;
 }
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = 'white')
+}
